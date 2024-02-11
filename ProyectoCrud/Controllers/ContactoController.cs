3d4e14a2d3f265f06bab929796f85bdc1e540ac5
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-02-11 13:19:55 -0300

    Metodo Inicio y Actualizar listo

--- ProyectoCrud/Controllers/ContactoController.cs
+++ ProyectoCrud/Controllers/ContactoController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.SqlClient;
 using System.Data;
+using System.Diagnostics.Contracts;
 
 
 namespace ProyectoCrud.Controllers
@@ -15,12 +16,20 @@ namespace ProyectoCrud.Controllers
     {
         private static string conexion = ConfigurationManager.ConnectionStrings["cadena"].ToString();
 
-        private List<Contacto> olista = new List<Contacto>();
+        private static List<Contacto> olista = new List<Contacto>();
+        /*
+         Problema: Cada vez que se realizaba una nueva solicitud HTTP a tu controlador, se creaba una nueva instancia de ContactoController, y con ella, una nueva olista. Esto significaba que cualquier cambio o adición a la lista se perdía entre solicitudes, a menos que esos datos se almacenaran y recuperaran de una fuente persistente como una base de datos.
+         Solución: Al hacer olista estática, ahora la lista es compartida por todas las instancias del controlador. Esto significa que cualquier contacto añadido a la lista permanece disponible en solicitudes HTTP subsiguientes, simulando un estado persistente.         
+         */
 
         // GET: Contacto
         public ActionResult Inicio()
         {
-            using (SqlConnection oconexion = new SqlConnection(conexion) )
+            //Evita q se agregen elementos repetidos a la lista
+            olista = new List<Contacto>();
+            
+
+            using (SqlConnection oconexion = new SqlConnection(conexion))
             {
                 SqlCommand cmd = new SqlCommand("SELECT * FROM CONTACTO", oconexion);
                 cmd.CommandType = CommandType.Text;
@@ -40,11 +49,70 @@ namespace ProyectoCrud.Controllers
 
                         olista.Add(nuevoContacto);
                     }
-                }               
-                                
+                }
+
             }
 
             return View(olista);
+        }
+
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Contacto contacto)
+        {
+
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("SP_Registrar", oconexion);
+                cmd.Parameters.AddWithValue("@Nombres", contacto.Nombres);
+                cmd.Parameters.AddWithValue("@Apellidos", contacto.Apellidos);
+                cmd.Parameters.AddWithValue("@Telefono", contacto.Telefono);
+                cmd.Parameters.AddWithValue("@Correo", contacto.Correo);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+
+                return RedirectToAction("Inicio");
+        }
+
+        [HttpGet]
+        public ActionResult Actualizar(int? id)
+        {
+            //la lista debe ser static para persistir los datos
+
+            if(id == null) return RedirectToAction("Inicio");
+
+            Contacto contacto = olista.Where(x => x.IdContacto == id).FirstOrDefault();
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        public ActionResult Actualizar(Contacto contacto)
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cdm = new SqlCommand("SP_Editar", oconexion);
+                cdm.Parameters.AddWithValue("@IdContacto", contacto.IdContacto);
+                cdm.Parameters.AddWithValue("@Nombres", contacto.Nombres);
+                cdm.Parameters.AddWithValue("@Apellidos", contacto.Apellidos);
+                cdm.Parameters.AddWithValue("@Telefono", contacto.Telefono);
+                cdm.Parameters.AddWithValue("@Correo", contacto.Correo);
+
+                cdm.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cdm.ExecuteNonQuery();
+            }
+
+                return RedirectToAction("Inicio");
         }
     }
 }
